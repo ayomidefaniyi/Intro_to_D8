ca5c0fa33e5fa4238b48953749374207950652ce
faniyi ayomideexit <faniyia25@gmail.com>
2025-08-26 16:08:26 +0100

    Add task 4 table

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,5 @@
 -- Print full description of books table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
-  INSERT INTO books (book_id, title, author_id, price)
-VALUES (1, 'Introduction to Databases', 1, 39.99);
-SHOW CREATE TABLE books;
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = DATABASE();
